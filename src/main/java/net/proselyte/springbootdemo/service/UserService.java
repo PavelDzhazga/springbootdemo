be2bd74ab195337777ec542c0fc78b8cdf3b3c0a
Pavel Dzhazga <dzhazga2002@mail.ru>
2023-03-06 16:22:13 +0300

    fix

--- src/main/java/net/proselyte/springbootdemo/service/UserService.java
+++ src/main/java/net/proselyte/springbootdemo/service/UserService.java
@@ -4,6 +4,7 @@ import net.proselyte.springbootdemo.model.User;
 import net.proselyte.springbootdemo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,18 +18,22 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    @Transactional
     public User findById(Long id){
         return userRepository.getOne(id);
     }
 
+    @Transactional(readOnly = true)
     public List<User> findAll(){
         return userRepository.findAll();
     }
 
+    @Transactional
     public User saveUser(User user){
         return userRepository.save(user);
     }
 
+    @Transactional
     public void deleteById(Long id){
         userRepository.deleteById(id);
     }
